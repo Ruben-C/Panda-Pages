5abad35cc946b7eddabb761a3414581ff792e932
Ruben Castaneda <rc2k7@rcbox.org>
2020-05-02 10:39:20 -0700

    Updates

---
+++ precache-manifest.ed8e2e38a823028271622f3f6fcda4dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda4da65d54b36d8b7f655b8995a62f0",
+    "url": "/Panda-Pages/index.html"
+  },
+  {
+    "revision": "1facc9b2eafa9f386964",
+    "url": "/Panda-Pages/static/css/main.bcf46939.chunk.css"
+  },
+  {
+    "revision": "1f0f40fc174b119dac58",
+    "url": "/Panda-Pages/static/js/2.7ccb3a28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Panda-Pages/static/js/2.7ccb3a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1facc9b2eafa9f386964",
+    "url": "/Panda-Pages/static/js/main.66cc32cd.chunk.js"
+  },
+  {
+    "revision": "c238de1666577336d300",
+    "url": "/Panda-Pages/static/js/runtime-main.6b64b4fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Panda-Pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
